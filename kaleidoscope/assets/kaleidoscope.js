329ca03193443f68f40e4a802ea345519086523b
Sergej Chodarev <sergej.chodarev@yandex.com>
2017-08-09 15:53:25 +0200

    Include PhotoSwipe elements directly in HTML

--- kaleidoscope/assets/kaleidoscope.js
+++ kaleidoscope/assets/kaleidoscope.js
@@ -17,8 +17,7 @@ function collectItems($thumbnails) {
 }
 
 function initPhotoSwipe() {
-    var $pswp = $('<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"><div class="pswp__bg"></div><div class="pswp__scroll-wrap"><div class="pswp__container"><div class="pswp__item"></div><div class="pswp__item"></div><div class="pswp__item"></div></div><div class="pswp__ui pswp__ui--hidden"><div class="pswp__top-bar"><div class="pswp__counter"></div><button class="pswp__button pswp__button--close" title="Close (Esc)"></button><button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button><button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button><div class="pswp__preloader"><div class="pswp__preloader__icn"><div class="pswp__preloader__cut"><div class="pswp__preloader__donut"></div></div></div></div></div><div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap"><div class="pswp__share-tooltip"></div></div><button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button><button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button><div class="pswp__caption"><div class="pswp__caption__center"></div></div></div></div></div>');
-    $pswp.appendTo("body");
+    var pswp = document.querySelector('.pswp')
     var $thumbnails = $('.thumbnail');
     var items = collectItems($thumbnails);
 
@@ -34,9 +33,7 @@ function initPhotoSwipe() {
                 return {x: offset.left, y: offset.top, w: $thumbnail.width()};
             }
         };
-
-        var photoSwipe = new PhotoSwipe($pswp.get(0), PhotoSwipeUI_Default, items, options);
-        photoSwipe.init();
+        new PhotoSwipe(pswp, PhotoSwipeUI_Default, items, options).init();
     });
 }
 
